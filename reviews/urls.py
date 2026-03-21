3b004c148eeeca399016cf65ba599c155cff431a
cromartie1020 <hrc245@gmail.com>
2026-03-21 15:37:21 +0000

    initial commit

---
+++ reviews/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('books/', views.book_list, name='book_list'),
+    path('books/<int:book_id>/', views.book_detail, name='book_detail'),
+]
---
+++ reviews/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from .utils import average_rating
+from .models import Review, Book,  Publisher, Review,  Profile
+def book_list(request):
+    books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+            book_list.append({'book': book, 'book_rating': book_rating, 'number_of_reviews': number_of_reviews})
+    books = Book.objects.all()
+    context = {
+        'books':books,
+        'book_list': book_list,
+    }
+    return render(request, 'reviews/book_list.html', context)
+
+def index(request):
+    return render(request, 'reviews/index.html')
+
+def book_detail(request, book_id):
+    try:
+        review = Review.objects.get(id=book_id)
+    except Review.DoesNotExist:
+        review = None
+
+    context = {
+        'review':review
+    }
+
+    return render(request, 'reviews/book_detail.html', context)
